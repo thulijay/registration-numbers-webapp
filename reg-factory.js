75d663b1b05f0df88d119b60d2a73cb7cec39af1
thulijay <thulijay79@gmail.com>
2020-11-22 13:06:00 +0200

    added tests

--- reg-factory.js
+++ reg-factory.js
@@ -2,45 +2,47 @@ module.exports = function registrationNumbers(pool) {
 
     async function workFlow(regEntry) {
 
-        regEntry = regEntry.toUpperCase();
+        townSub = regEntry.substring(0,2)
 
-        let townId = await getTownId(regEntry.split(" ")[0]);
+        let regSub = await getTownId(townSub);
 
 
-        if (regEntry.length <= 10) {
             let selectRegistration = await pool.query('select reg_number from registration_num where reg_number = $1', [regEntry]);
         
         if (selectRegistration.rowCount === 0) {
-            await pool.query('insert into registration_num (reg_number, town_id) values ($1,$2)', [regEntry, townId]);
+            await pool.query('insert into registration_num (reg_number, town_id) values ($1,$2)', [regEntry, regSub]);
         }
-            return false;
-        }
+        
     }
-
 
     async function insertData(insertReg){
         let INSERT_QUERY = await pool.query('insert into registration_num (reg_number) values ($1)', [insertReg]);
         return INSERT_QUERY.rows;
     }
 
-    async function duplicateReg(regEntry) {
-        let CHECK_QUERY = await pool.query('select reg_number from registration_num where reg_number = $1', [regEntry]);
+    async function duplicateReg(dupReg) {
+        let CHECK_QUERY = await pool.query('select reg_number from registration_num where reg_number = $1', [dupReg]);
         return CHECK_QUERY.rowCount;
     }
 
     async function getTownId(towns) {
         let REG_QUERY = await pool.query("select id from town where reg_id = $1", [towns]);
-        return REG_QUERY.rows[0]['id'];
+        return REG_QUERY.rows[0].id;
     }
 
     async function townOutcome() {
-        let TOWNS_QUERY = await pool.query('select reg_number from registration_num');
+        let TOWNS_QUERY = await pool.query('select reg_number from registration_num ');
         return TOWNS_QUERY.rows;
     }
 
     async function resetBtn() {
         await pool.query('DELETE FROM registration_num');
     }
+
+    // async function townSelection(){
+    //     let townQuery = await pool.query('select reg_number from registration_num');
+    //     return townQuery.rows;
+    // }
 
     async function getReg(regId) {
         let GET_QUERY = await pool.query('select reg_number from registration_num where town_id = $1', [regId]);
@@ -60,7 +62,7 @@ module.exports = function registrationNumbers(pool) {
             let filteredTown = await townOutcome();
             return filteredTown;
         } else {
-            let testing = await getReg(filterReg);
+            let testing = await townOutcome(filterReg);
             return testing;
         }
     }
@@ -72,6 +74,7 @@ module.exports = function registrationNumbers(pool) {
         filterBtn,
         resetTownBtn,
         insertData,
-        getReg
+        getReg,
+        getTownId
     }
 }
--- test/reg-tests.js
+++ test/reg-tests.js
@@ -1,71 +1,143 @@
 const assert = require('assert');
 const reg = require('../reg-factory');
 const regFactory = require('../reg-factory');
-describe('Testing Registration Numbers', function(){
+describe('Testing Registration Numbers', function () {
 
 
-const pg = require('pg');
-const Pool = pg.Pool;
-const connectionString = process.env.DATABASE_URL || 'postgres://melissa:pg123@localhost:5432/reg_tests';
-const pool = new Pool({
+  const pg = require('pg');
+  const Pool = pg.Pool;
+  const connectionString = process.env.DATABASE_URL || 'postgres://melissa:pg123@localhost:5432/reg_tests';
+  const pool = new Pool({
     connectionString
-})
-const flow = reg(pool);
+  })
+  const flow = reg(pool);
 
-beforeEach(async function(){        
+  beforeEach(async function () {
     await pool.query('delete from registration_num');
-});
+  });
 
-    it('should be not take a number plate from any other place besides CF, CY, CL and CA', async function(){
-            let flow = reg(pool);
-    
-        assert.equal(false,await flow.townOutcome('CAA 458 566'));
+  it('should be not take a number plate from any other place besides CF, CY, CL and CA', async function () {
+    let flow = reg(pool);
 
-    })
+    assert.equal(false, await flow.townOutcome('CAA 458 566'));
 
-    it('should be able to add a registration number from Kraaifontein', async function(){
-        let flow = reg(pool);
+  })
 
-          await flow.getReg();
-             let outReg = await flow.townOutcome('CF 789 561');
-    
-            assert.equal(undefined, outReg.rows)
-    })
+  it('should be able to add a registration number from Kraaifontein', async function () {
+    let flow = reg(pool);
 
-    it('should be able to add a registration number from Bellville', async function(){
-        let flow = reg(pool);
+    await flow.workFlow('CF 689 561');
+    await flow.workFlow('CF 745 789');
+    await flow.workFlow('CF 145 208');
 
-          await flow.getReg();
-             let outReg = await flow.townOutcome('CY 896 561');
-    
-            assert.equal(undefined, outReg.rows)
-    })
+    let outReg = await flow.townOutcome();
 
-    it('should be able to add a registration number from Stellenbosh', async function(){
-        let flow = reg(pool);
+    assert.deepStrictEqual(outReg, [{ reg_number: 'CF 689 561' },
+    { reg_number: 'CF 745 789' },
+    { reg_number: 'CF 145 208' }
+    ])
+  })
 
-          await flow.getReg();
-             let outReg = await flow.townOutcome('CL 145 666');
-    
-            assert.equal(undefined, outReg.rows)
-    })
-    it('should be able to add a registration number from Cape Town', async function(){
-        let flow = reg(pool);
+  it('should be able to add a registration number from Bellville', async function () {
+    let flow = reg(pool);
 
-          await flow.getReg();
-             let outReg = await flow.townOutcome('CA 745 561');
-    
-            assert.equal(undefined, outReg.rows)
-    })
-    it('should be able to clear data', async function(){
-        let flow = reg(pool);
+    await flow.workFlow('CY 689 561');
+    await flow.workFlow('CY 745 789');
+    await flow.workFlow('CY 145 208');
 
-        await flow.townOutcome('CF 478 455');
-        await flow.townOutcome('CL 878 455');
-        await flow.townOutcome('CY 478 455');
-        await flow.townOutcome('CA 478 455');
+    let outReg = await flow.townOutcome();
 
-        assert.equal(await flow.resetBtn(), undefined)
-    })
-    
+    assert.deepStrictEqual(outReg, [{ reg_number: 'CY 689 561' },
+    { reg_number: 'CY 745 789' },
+    { reg_number: 'CY 145 208' }
+    ])
+  })
+
+  it('should be able to add a registration number from Stellenbosh', async function () {
+    let flow = reg(pool);
+
+    await flow.workFlow('CL 689 561');
+    await flow.workFlow('CL 745 789');
+    await flow.workFlow('CL 145 208');
+
+    let outReg = await flow.townOutcome();
+
+    assert.deepStrictEqual(outReg, [{ reg_number: 'CL 689 561' },
+    { reg_number: 'CL 745 789' },
+    { reg_number: 'CL 145 208' }
+    ])
+  })
+
+  it('should be able to add a registration number from Cape Town', async function () {
+    let flow = reg(pool);
+
+    await flow.workFlow('CA 689 561');
+    await flow.workFlow('CA 745 789');
+    await flow.workFlow('CA 145 208');
+
+    let outReg = await flow.townOutcome();
+
+    assert.deepStrictEqual(outReg, [{ reg_number: 'CA 689 561' },
+    { reg_number: 'CA 745 789' },
+    { reg_number: 'CA 145 208' }])
+  })
+
+  it('should be able to clear data', async function () {
+    let flow = reg(pool);
+
+    await flow.townOutcome('CF 478 455');
+    await flow.townOutcome('CL 878 455');
+    await flow.townOutcome('CY 478 455');
+    await flow.townOutcome('CA 478 455');
+
+    assert.equal(await flow.resetBtn(), undefined)
+  })
+
+  it('should only filter Cape Town', async function () {
+    let flow = reg(pool);
+
+    await flow.workFlow('CA 689 561');
+    await flow.workFlow('CL 745 789');
+    await flow.workFlow('CL 145 208');
+
+    let outReg = await flow.filterBtn(8);
+// console.log(outReg)
+    assert.deepEqual(outReg, [{ reg_number: 'CA 689 561' }])
+  })
+
+  it('should only filter Kraaifontein', async function () {
+    let flow = reg(pool);
+
+    await flow.workFlow('CF 889 561');
+    await flow.workFlow('CA 745 789');
+    await flow.workFlow('CL 145 208');
+
+    let outReg = await flow.filterBtn(5);
+
+    assert.deepEqual(outReg, [{ reg_number: 'CF 889 561' }])
+  })
+
+  it('should only filter Stellenbosch', async function () {
+    let flow = reg(pool);
+
+    await flow.workFlow('CL 689 561');
+    await flow.workFlow('CA 745 789');
+    await flow.workFlow('CY 145 208');
+
+    let outReg = await flow.filterBtn(7);
+// console.log(outReg)
+    assert.deepEqual(outReg, [{ reg_number: 'CL 689 561' }])
+  })
+
+  it('should only filter Belville', async function () {
+    let flow = reg(pool);
+
+    await flow.workFlow('CY 689 561');
+    await flow.workFlow('CA 745 789');
+    await flow.workFlow('CL 145 208');
+
+    let outReg = await flow.filterBtn(6);
+// console.log(outReg)
+    assert.deepEqual(outReg, [{ reg_number: 'CY 689 561' }])
+  })
 })
